6c4ae997b3b935ba2d8b9d8fb355e3bed010bbd5
Virendra Sirture <virendrasirture05@gmail.com>
2024-10-15 13:14:08 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import store from "./redux/store.js";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";;
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import store from './redux/store';
+import store from './redux/store.js';
 
 
 createRoot(document.getElementById("root")).render(
